cbddb78440235102563569685d36999b3ffca820
Owusu Ansah Joy <owusujoyansah@gmail.com>
2024-06-29 14:47:50 -0700

    Advance

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 #making changes to config file
 file {'/etc/ssh/ssh_config':
-	ensure => 'present',
+  ensure => 'present',
 },
 
 file_line {'Turn off password auth':
-	path => '/etc/ssh/ssh_config',
-	line => 'PasswordAuthentication no',
-	match => 'PasswordAuthnetication yes',
-	replace => 'true',
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+  match => 'PasswordAuthnetication yes',
+  replace => 'true',
 }
 
 file_line {'Use an I dentity file':
-	path => '/etc/ssh/ssh_config',
-        line => 'IdentityFile ~/.ssh/config',
-        match => 'IdentityFile',
-        ensure => 'true',
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/config',
+  match => 'IdentityFile',
+  ensure => 'true',
 }
